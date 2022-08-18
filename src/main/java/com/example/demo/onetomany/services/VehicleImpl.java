c0cf28e3d66d9b67911aff24638cd99529c15e54
Vipin Regulwar <vipinrglwr271@gmail.com>
2022-08-18 13:11:56 +0530

    Store procedure

---
+++ src/main/java/com/example/demo/onetomany/services/VehicleImpl.java
@@ -0,0 +1,22 @@
+//package com.example.demo.onetomany.services;
+//
+//import com.example.demo.onetomany.entity.Vehicle;
+//import com.example.demo.onetomany.repository.VehicleRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@Service
+//public class VehicleImpl implements VehicleService {
+//
+//    @Autowired
+//    private VehicleRepository vehicleRepository;
+//
+//    @Override
+//    public String saveVehicle(Vehicle vehicle){
+//        Vehicle vehicle1 = vehicleRepository.save(vehicle);
+//        return "Vehicle Save ";
+//    }
+//
+//
+//}
---
+++ src/main/java/com/example/demo/onetomany/services/VehicleService.java
@@ -0,0 +1,5 @@
+//package com.example.demo.onetomany.services;
+//
+//public interface VehicleService {
+//
+//}
